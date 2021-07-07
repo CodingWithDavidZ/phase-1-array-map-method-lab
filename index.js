50f783960e809daa65fb9e21fc4e77e423ee712a
David Zombar <davidzombar@gmail.com>
2021-07-07 09:05:00 -0600

    commit tree 172dd4e54cc00c610296d3c85e2921553d4bf223

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(makeTitleCased);
+};
+
+function makeTitleCased(arr) {
+  return arr
+    .split(" ")
+    .map(function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    })
+    .join(" ");
 }
